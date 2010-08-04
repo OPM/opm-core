235f534376817e38a24633ba4e0f3f10f6e0a283
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2010-08-04 11:51:58 +0000

    Appease MLINT.

--- test_mex_schur_comp_symm.m
+++ test_mex_schur_comp_symm.m
@@ -3,7 +3,7 @@ G = computeGeometry(cartGrid([2,1]));
 rock.perm = ones(G.cells.num, 1);
 BI = mex_ip_simple(G, rock);
 
-nconn = diff(G.cells.facePos)
+nconn = diff(G.cells.facePos);
 [S, r, F, L] = mex_schur_comp_symm(BI, nconn);
 
 [i, j] = blockDiagIndex(nconn, nconn);
@@ -12,4 +12,4 @@ SS = sparse(double(G.cells.faces(i, 1)), ...
             double(G.cells.faces(j, 1)),  S );
 R  = accumarray(G.cells.faces(:, 1), r);
 
-x = SS \ R
+x = SS \ R %#ok
