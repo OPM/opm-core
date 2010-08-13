2a308522571e883b2fa013cc15b656e150f6e860
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2010-08-13 17:24:19 +0000

    Add MEX'ed edition of 'partitionUI'.  The MEX function only   implements the first edition of the 'partitionUI' algorithm (i.e.,   before afg added sub-grid support in r2349), but it is nevertheless   a useful experiment and facilitates testing a C/C++ implementation   of the MsMFE method.

---
+++ mex_partition_ui.m
@@ -0,0 +1,42 @@
+function varargout = mex_partition_ui(varargin)
+%Partition grid uniformly in logical space using compiled C code.
+%
+% SYNOPSIS:
+%   p = mex_partition_ui(ix, fineDim, coarseDim)
+%
+% PARAMETERS:
+%   ix        - Indices, e.g., G.cells.indexMap.
+%
+%   fineDim   - Cartesian dimensions of underlying index space (e.g.
+%               G.cartDims)
+%
+%   coarseDim - Cartesian dimensions of requested index space.  Typically,
+%               all(coarseDim <= fineDim).
+%
+% RETURNS:
+%   p - Partition vector of SIZE(ix).
+%
+% SEE ALSO:
+%   partitionUI.
+
+%{
+#COPYRIGHT#
+%}
+
+% $Date$
+% $Revision$
+
+   buildmex CFLAGS="\$CFLAGS -Wall -Wextra -ansi -pedantic           ...
+        -Wformat-nonliteral -Wcast-align -Wpointer-arith             ...
+        -Wbad-function-cast -Wmissing-prototypes -Wstrict-prototypes ...
+        -Wmissing-declarations -Winline -Wundef -Wnested-externs     ...
+        -Wcast-qual -Wshadow -Wconversion -Wwrite-strings            ...
+        -Wno-conversion -Wchar-subscripts -Wredundant-decls"         ...
+   ...
+        -g -largeArrayDims -DCOMPILING_FOR_MATLAB=1    ...
+   ...
+        mex_partition_ui.c partition.c
+
+   % Call MEX'ed edition.
+   [varargout{1:nargout}] = mex_partition_ui(varargin{:});
+end
