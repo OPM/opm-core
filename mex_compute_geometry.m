8257bc51aaa67693eaf22bc82777934a7c65ed86
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2010-09-07 10:42:38 +0000

    Move geometry support to separate module.

--- mex_compute_geometry.m
+++
@@ -1,8 +0,0 @@
-function varargout = mex_compute_geometry(varargin)
-   buildmex -O -largeArrayDims                           ...
-            mex_compute_geometry.c geometry.c mrst_api.c ...
-            -lm
-
-   % Call MEX'ed edition.
-   [varargout{1:nargout}] = mex_compute_geometry(varargin{:});
-end
