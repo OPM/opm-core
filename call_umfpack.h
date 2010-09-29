65938abe930387f6b8587b490a1aacaa40a1b160
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2010-09-29 09:42:03 +0000

    Split MEX support out of libmimetic and into two separate dirs.

--- call_umfpack.h
+++
@@ -1,6 +0,0 @@
-#ifndef CALL_UMFPACK_H_INCLUDED
-#define CALL_UMFPACK_H_INCLUDED
-void
-callMWUMFPACK(int n, mwSignedIndex* ia, mwSignedIndex* ja, 
-              double *sa, double *b, double *x);
-#endif
