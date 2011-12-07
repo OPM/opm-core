1e7d06efb4f19082f30d16ed3164c0ad9f7a64b2
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2011-12-07 14:16:21 +0100

    Parameter subsystem now uses tinyxml instead of libxml2.

--- dune/common/param/ParameterXML.cpp
+++ dune/common/param/ParameterXML.cpp
@@ -45,6 +45,99 @@ along with OpenRS.  If not, see <http://www.gnu.org/licenses/>.
 #include <string>
 #include <boost/filesystem.hpp>
 
+#if 1 // Use tinyxml
+
+#include <dune/common/param/tinyxml/tinyxml.h>
+
+
+
+namespace Dune {
+    namespace parameter {
+
+	namespace tinyxml {
+	    void read_xml(ParameterGroup& pg, const std::string filename);
+	    void fill_tree(ParameterGroup& pg,
+                           const TiXmlNode* root,
+                           const std::string& xml_file_path);
+	}
+
+	void fill_xml(ParameterGroup& pg, const std::string filename) {
+	    tinyxml::read_xml(pg, filename);
+	}
+
+	namespace tinyxml {
+	    std::string getProperty(const std::string& property,
+                                    const TiXmlElement* node_ptr)
+            {
+		const char* prop_value_ptr = node_ptr->Attribute(property.c_str());
+		std::string property_value(prop_value_ptr ? prop_value_ptr : "");
+		return property_value;
+	    }
+
+	    void read_xml(ParameterGroup& pg, const std::string filename)
+            {
+                TiXmlDocument doc(filename);
+                bool ok = doc.LoadFile();
+                if (!ok) {
+		    std::cerr << "ERROR: Failed to open XML file '" << filename << "'\n";
+		    throw std::exception();
+                }
+                const TiXmlNode* root = doc.RootElement();
+		std::string xml_file_path = boost::filesystem::path(filename).branch_path().string();
+		fill_tree(pg, root, xml_file_path);
+	    }
+
+	    void fill_tree(ParameterGroup& pg,
+                           const TiXmlNode* root,
+                           const std::string& xml_file_path)
+            {
+		//std::cout << "GROUP '" << value << "' BEGIN\n";
+		for (const TiXmlNode* cur = root->FirstChild(); cur; cur = cur->NextSibling()) {
+                    const TiXmlElement* elem = cur->ToElement();
+		    if (elem) {
+			std::string tag_name = elem->ValueStr();
+			if (tag_name == ID_xmltag__file_params) {
+			    std::string relative_filename = getProperty(ID_xmlatt__value, elem);
+			    std::string filename = (boost::filesystem::path(xml_file_path) / relative_filename).string();
+			    fill_xml(pg, filename);
+			    continue;
+			}
+			std::string name = getProperty(ID_xmlatt__name, elem);
+			std::tr1::shared_ptr<ParameterMapItem> data;
+			if (tag_name == ID_xmltag__param) {
+			    std::string value = getProperty(ID_xmlatt__value, elem);
+			    std::string type = getProperty(ID_xmlatt__type, elem);
+			    if (type == ID_param_type__file) {
+				value = (boost::filesystem::path(xml_file_path) / value).string();
+				type  = ID_param_type__string;
+			    }
+			    data.reset(new Parameter(value, type));
+			} else if (tag_name == ID_xmltag__param_grp) {
+			    std::string child_path  = pg.path() + ID_delimiter_path + name;
+			    data.reset(new ParameterGroup(child_path, &pg));
+			    fill_tree(dynamic_cast<ParameterGroup&>(*data), elem, xml_file_path);
+			} else if (tag_name == ID_xmltag__file_param_grp) {
+			    std::string child_path  = pg.path() + ID_delimiter_path + name;
+			    data.reset(new ParameterGroup(child_path, &pg));
+			    std::string relative_filename = getProperty(ID_xmlatt__value, elem);
+			    std::string filename = (boost::filesystem::path(xml_file_path) / relative_filename).string();
+			    fill_xml(dynamic_cast<ParameterGroup&>(*data), filename);
+			} else {
+			    std::cerr << "ERROR: '" << tag_name << "' is an unknown xml tag.\n";
+			    throw std::exception();
+			}
+			pg.insert(name, data);
+		    }
+		}
+		//std::cout << "GROUP '" << id << "' END\n";
+	    }
+	}
+    } // namespace parameter
+} // namespace Dune
+
+
+#else // Use libxml2
+
 #include <libxml/parser.h> // libxml2
 #include <libxml/tree.h>   // libxml2
 
@@ -138,3 +231,6 @@ namespace Dune {
 	}
     } // namespace parameter
 } // namespace Dune
+
+
+#endif
