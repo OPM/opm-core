ad7c16cb612cc1b38cdd521329b95271168f9338
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2011-12-08 16:31:47 +0100

    Set initial 'facepos' member lest the contents be undefined.
    Detected by: valgrind.

--- tests/test_cfs_tpfa.c
+++ tests/test_cfs_tpfa.c
@@ -223,6 +223,9 @@ cart_grid(int nx, int ny, int nz)
     cfacepos   = G->cell_facepos;
     ccentroids = G->cell_centroids;
     cvolumes   = G->cell_volumes;
+
+    cfacepos[0] = 0;
+
     for (k=0; k<nz; ++k)  {
         for (j=0; j<ny; ++j) {
             for (i=0; i<nx; ++i) {
