5e049918b73c9779b960b6eb7631ed0349832648
Jostein R. Natvig <jostein.r.natvig@sintef.no>
2012-01-16 13:31:39 +0100

    Do not check monotonicity of ZCORN in inactive cells.

--- opm/core/utility/cpgpreprocess/preprocess.c
+++ opm/core/utility/cpgpreprocess/preprocess.c
@@ -457,8 +457,13 @@ void process_grdecl(const struct grdecl   *in,
 	  double z1 = sign*in->zcorn[i+2*nx*(j+2*ny*(k))];
 	  double z2 = sign*in->zcorn[i+2*nx*(j+2*ny*(k+1))];
 	  
-	  if (z2 < z1){
+          int c1 = i/2 + nx*(j/2 + ny*k/2);
+          int c2 = i/2 + nx*(j/2 + ny*(k+1)/2);
+          
+	  if (in->actnum[c1] && in->actnum[c2] && (z2 < z1)){
 	    fprintf(stderr, "\nZCORN should be strictly nondecreasing along pillars!\n");	 
+            fprintf(stderr, "(%d %d %d) %d %d\n%24.16f\n%24.16f: %d\n", 
+                    i,j,k,in->actnum[c1], in->actnum[c2], z1, z2, z2<z1);
 	    error = 1;
 	    goto end;
 	  }
