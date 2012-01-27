1600f1b4caccb2a66518aa0f3c792c1a702dd93b
Jostein R. Natvig <jostein.r.natvig@sintef.no>
2012-01-27 12:25:31 +0000

    Replace automatic build system with more warnings turned on-
    Signed-off-by: Bård Skaflestad <Bard.Skaflestad@sintef.no>

--- processgrid_mex.m
+++ processgrid_mex.m
@@ -32,11 +32,31 @@ function varargout = processgrid_mex(varargin)
 % $Date$
 % $Revision$
 
-% Build MEX edition of same.
-%
-buildmex CFLAGS='$CFLAGS -Wall -fPIC' processgrid.c preprocess.c ...
-         uniquepoints.c facetopology.c sparsetable.c mxgrdecl.c  ...
-         -output processgrid_mex.mexa64
+   % Build MEX edition of same.
+   %
 
-% Call MEX edition.
-[varargout{1:nargout}] = processgrid_mex(varargin{:});
+   v = version;v = v([1,3]);
+
+   CFLAGS = {'CFLAGS="\$CFLAGS', '-g', '-Wall', '-Wextra', '-ansi', ...
+             '-pedantic', '-Wformat-nonliteral',  '-Wcast-align', ...
+             '-Wpointer-arith', '-Wbad-function-cast', ...
+             '-Wmissing-prototypes ', '-Wstrict-prototypes', ...
+             '-Wmissing-declarations', '-Winline', '-Wundef', ...
+             '-Wnested-externs', '-Wcast-qual', '-Wshadow', ...
+             '-Wconversion', '-Wwrite-strings', '-Wno-conversion', ...
+             '-Wchar-subscripts', '-Wredundant-decls"'};
+
+   SRC = {'processgrid.c', 'preprocess.c', 'uniquepoints.c', ...
+          'facetopology.c', 'sparsetable.c', 'mxgrdecl.c'};
+       
+   INCLUDE = {};
+   
+   OPTS = {'-output', ['processgrid_mex.', mexext], ...
+      '-largeArrayDims', ['-DMATLABVERSION=', v], '-g'};
+   
+   buildmex(CFLAGS{:}, INCLUDE{:}, OPTS{:}, SRC{:})
+
+   % Call MEX edition.
+   [varargout{1:nargout}] = processgrid_mex(varargin{:});
+end
+
