d59d648e93897453e02229c3dccd758ccb2954f1
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2012-04-24 16:39:32 +0200

    Allocate_cart_grid_2d(): Repeat cset a1537a0d194a from 3D case.

--- opm/core/grid/cart_grid.c
+++ opm/core/grid/cart_grid.c
@@ -543,7 +543,8 @@ allocate_cart_grid_2d(int nx, int ny)
         G->global_cell      = NULL;
         G->cell_facetag     = NULL;
 
-        if ((G->face_nodes       == NULL ) ||
+        if ((G->node_coordinates == NULL ) ||
+            (G->face_nodes       == NULL ) ||
             (G->face_nodepos     == NULL ) ||
             (G->face_cells       == NULL ) ||
             (G->face_centroids   == NULL ) ||
