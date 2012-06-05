831b2bb40bf558ce37271ef923846647bbb9a9b4
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2012-06-05 15:20:13 +0200

    Added skeleton doxygen_main.hpp for overview documentation. Also add generation of docs from opm/core, and disable docs from unfinished tutorial.

---
+++ opm/core/doxygen_main.hpp
@@ -0,0 +1,35 @@
+/*
+  Copyright 2012 SINTEF ICT, Applied Mathematics.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPM_DOXYGEN_MAIN_HEADER_INCLUDED
+#define OPM_DOXYGEN_MAIN_HEADER_INCLUDED
+
+
+/** \mainpage Documentation for the opm-core library.
+
+The following are the main library features:
+<h3>Grid handling</h3>
+<h3>Well handling</h3>
+<h3>Pressure solvers</h3>
+<h3>Transport solvers</h3>
+<h3>Various utilities</h3>
+
+*/
+
+#endif // OPM_DOXYGEN_MAIN_HEADER_INCLUDED
