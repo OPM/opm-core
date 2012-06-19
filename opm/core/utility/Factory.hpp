fbefe5d85479155fe3a79a410754537b6624056e
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2012-06-20 00:04:48 +0200

    Replace all <tab>s by (8) spaces.

--- opm/core/utility/Factory.hpp
+++ opm/core/utility/Factory.hpp
@@ -35,74 +35,73 @@
 #ifndef OPENRS_FACTORY_HEADER
 #define OPENRS_FACTORY_HEADER
 
-
 #include <map>
 #include <tr1/memory>
 
 namespace Opm
 {
-    
+
     /** This is an object factory for creating objects of some type
      *  requested by the user, with a shared base class.  The user
      *  need only interact with the factory through the static
      *  template member addCreator() and the static member function
      *  createObject().
      */
-    template <class Base>
+    template<class Base>
     class Factory
     {
     public:
         /// The type of pointer returned by createObject().
         typedef std::tr1::shared_ptr<Base> ProductPtr;
 
-	/// Creates a new object of the class associated with the given type string,
+        /// Creates a new object of the class associated with the given type string,
         /// and returns a pointer to it.
-	/// \param type the type string of the class that the user wants to have
-	///             constructed.
+        /// \param type the type string of the class that the user wants to have
+        ///             constructed.
         /// \return (smart) pointer to the created object.
-	static ProductPtr createObject(const std::string& type)
-	{
-	    return instance().doCreateObject(type);
-	}
+        static ProductPtr createObject(const std::string& type)
+        {
+            return instance().doCreateObject(type);
+        }
 
-	/// Clones an new object of the class associated with the given type string,
+        /// Clones an new object of the class associated with the given type string,
         /// and returns a pointer to it.
-	/// \param type the type string of the class that the user wants to have
-	///             constructed.
-	/// \param original (smart) pointer to object to be cloned.
+        /// \param type the type string of the class that the user wants to have
+        ///             constructed.
+        /// \param original (smart) pointer to object to be cloned.
         /// \return (smart) pointer to the created object.
-	static ProductPtr cloneObject(const std::string& type,
+        static ProductPtr cloneObject(const std::string& type,
                                       const ProductPtr original)
-	{
-	    return instance().doCloneObject(type, original);
-	}
+        {
+            return instance().doCloneObject(type, original);
+        }
 
-	/// Add a creator to the Factory.
+        /// Add a creator to the Factory.
         /// After the call, the user may obtain new objects of the Derived type by
         /// calling createObject() with the given type string as an argument.
         /// \tparam Derived the class we want to add a creator for, must inherit
         ///                 the class template parameter Base.
-	/// \param type the type string with which we want the Factory to associate
-	///             the class Derived.
-        template <class Derived>
-	static void addCreator(const std::string& type)
-	{
+        /// \param type the type string with which we want the Factory to associate
+        ///             the class Derived.
+        template<class Derived>
+        static void addCreator(const std::string& type)
+        {
             instance().doAddCreator<Derived>(type);
-	}
+        }
 
     private:
         // The method that implements the singleton pattern,
         // using the Meyers singleton technique.
-	static Factory& instance()
-	{
+        static Factory& instance()
+        {
             static Factory singleton;
-	    return singleton;
-	}
+            return singleton;
+        }
 
-	// Private constructor, to keep users from creating a Factory.
-	Factory()
-	{
-	}
+        // Private constructor, to keep users from creating a Factory.
+        Factory()
+        {
+        }
 
         // Abstract base class for Creators.
         class Creator
@@ -114,8 +113,8 @@ namespace Opm
         };
 
         /// This is the concrete Creator subclass for generating Derived objects.
-        template <class Derived>
-        class ConcreteCreator : public Creator
+        template<class Derived>
+        class ConcreteCreator: public Creator
         {
         public:
             virtual ProductPtr create()
@@ -132,45 +131,42 @@ namespace Opm
         typedef std::tr1::shared_ptr<Creator> CreatorPtr;
         typedef std::map<std::string, CreatorPtr> CreatorMap;
         // This map contains the whole factory, i.e. all the Creators.
-	CreatorMap string_to_creator_;
+        CreatorMap string_to_creator_;
 
         // Actually creates the product object.
-	ProductPtr doCreateObject(const std::string& type)
-	{
-	    typename CreatorMap::iterator it;
-	    it = string_to_creator_.find(type);
-	    if (it == string_to_creator_.end()) {
-		THROW("Creator type " << type
-		      << " is not registered in the factory.");
-	    }
-	    return it->second->create();
-	}
+        ProductPtr doCreateObject(const std::string& type)
+        {
+            typename CreatorMap::iterator it;
+            it = string_to_creator_.find(type);
+            if (it == string_to_creator_.end()) {
+                THROW("Creator type " << type
+                      << " is not registered in the factory.");
+            }
+            return it->second->create();
+        }
 
         // Actually clones the product object.
-	ProductPtr doCloneObject(const std::string& type,
+        ProductPtr doCloneObject(const std::string& type,
                                  const ProductPtr original)
-	{
-	    typename CreatorMap::iterator it;
-	    it = string_to_creator_.find(type);
-	    if (it == string_to_creator_.end()) {
-		THROW("Creator type " << type
-		      << " is not registered in the factory.");
-	    }
-	    return it->second->clone(original);
-	}
+        {
+            typename CreatorMap::iterator it;
+            it = string_to_creator_.find(type);
+            if (it == string_to_creator_.end()) {
+                THROW("Creator type " << type
+                      << " is not registered in the factory.");
+            }
+            return it->second->clone(original);
+        }
 
         // Actually adds the creator.
-        template <class Derived>
+        template<class Derived>
         void doAddCreator(const std::string& type)
         {
             CreatorPtr c(new ConcreteCreator<Derived>);
-	    string_to_creator_[type] = c;
+            string_to_creator_[type] = c;
         }
     };
-
 
 } // namespace Opm
 
 #endif // OPENRS_FACTORY_HEADER
-
-
