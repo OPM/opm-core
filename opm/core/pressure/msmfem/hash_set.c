f77a7c19ae8cd280b2088c511913f8703352b7b0
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2012-06-22 19:39:52 +0200

    Reference module headers from canonical location.

--- opm/core/pressure/msmfem/hash_set.c
+++ opm/core/pressure/msmfem/hash_set.c
@@ -23,7 +23,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "hash_set.h"
+#include <opm/core/pressure/msmfem/hash_set.h>
 
 /* ======================================================================
  * Macros
