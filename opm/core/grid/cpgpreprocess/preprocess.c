462c7cf8e3ebf5f4fcb5b75225499363fd9ee5f5
Ivar Ursin Nikolaisen <ivarun@idi.ntnu.no>
2012-08-21 15:15:40 +0200

    Add parentheses to c1 assignment for consistency.

--- opm/core/grid/cpgpreprocess/preprocess.c
+++ opm/core/grid/cpgpreprocess/preprocess.c
@@ -587,7 +587,7 @@ get_zcorn_sign(int nx, int ny, int nz, const int *actnum,
                     z1 = sign*zcorn[i+2*nx*(j+2*ny*(k))];
                     z2 = sign*zcorn[i+2*nx*(j+2*ny*(k+1))];
 
-                    c1 = i/2 + nx*(j/2 + ny*k/2);
+                    c1 = i/2 + nx*(j/2 + ny*(k/2));
                     c2 = i/2 + nx*(j/2 + ny*((k+1)/2));
 
                     if (((actnum == NULL) ||
