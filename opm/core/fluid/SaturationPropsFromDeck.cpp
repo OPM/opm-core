95fac1074b6d913b994693b2ba7887b6c767448f
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2012-09-04 13:15:26 +0200

    Constified some local variables.

--- opm/core/fluid/SaturationPropsFromDeck.cpp
+++ opm/core/fluid/SaturationPropsFromDeck.cpp
@@ -142,7 +142,7 @@ namespace Opm
         }
 
         // Initialize tables.
-        int tab_size = param.getDefault("tab_size_kr", 200);
+        const int tab_size = param.getDefault("tab_size_kr", 200);
         satfuncset_.resize(num_tables);
         for (int table = 0; table < num_tables; ++table) {
             satfuncset_[table].init(deck, table, phase_usage_, tab_size);
