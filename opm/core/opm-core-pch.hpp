f4350098ed23f82e7b9ea3b7df5fa88c4a035acf
Roland Kaufmann <roland.kaufmann@uni.no>
2013-01-23 14:35:50 +0100

    Use precompiled headers to compile C++ modules
    On an average system this will cut around 15% of the total build time.
    Unfortunately, including Boost headers in the precompiled header takes
    longer time to generate and then read in each module, than just
    including the necessary headers in each module.
    
    Use Noel Llopis' list_precomp.py at
    http://www.gamesfromwithin.com/wp-content/uploads/bin/list_precomp_py.txt
    to analyse which headers are included the most and are candidates for
    inclusion.

---
+++ opm/core/opm-core-pch.hpp
@@ -0,0 +1,10 @@
+/* List of headers that should be precompiled */
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <string>
+#include <vector>
