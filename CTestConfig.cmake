2cc9807b6395b4a9ec9b3df10a24f06a266fa98e
Andreas Lauser <Andreas.Lauser@iws.uni-stuttgart.de>
2013-01-24 14:26:00 +0100

    Enable CDash testing
    Configure everything using CMake and run 'ctest -D Experimental'
    inside the build directory. If you want to participate in the nightly
    testing, add
    
    	#!/bin/sh
    	cd $BUILD_DIR
    	exec ctest -D Nightly
    
    to /etc/cron.daily/opm-core. The results can then be browsed at
    
    <http://opm-project.org/CDash/index.php?project=opm-core>

---
+++ CTestConfig.cmake
@@ -0,0 +1,6 @@
+set(CTEST_PROJECT_NAME "opm-core")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "opm-project.org")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=opm-core")
+set(CTEST_DROP_SITE_CDASH TRUE)
