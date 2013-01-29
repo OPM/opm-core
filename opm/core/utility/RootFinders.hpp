8e30215f16e800ee3826054d515d195bd807b442
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2013-01-29 13:17:01 +0100

    Changed OpenRS->OPM in copyright notices and #include guards.

--- opm/core/utility/RootFinders.hpp
+++ opm/core/utility/RootFinders.hpp
@@ -17,24 +17,24 @@
   Copyright 2010 SINTEF ICT, Applied Mathematics.
   Copyright 2010 Statoil ASA.
 
-  This file is part of The Open Reservoir Simulator Project (OpenRS).
+  This file is part of the Open Porous Media project (OPM).
 
-  OpenRS is free software: you can redistribute it and/or modify
+  OPM is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  OpenRS is distributed in the hope that it will be useful,
+  OPM is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with OpenRS.  If not, see <http://www.gnu.org/licenses/>.
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef OPENRS_ROOTFINDERS_HEADER
-#define OPENRS_ROOTFINDERS_HEADER
+#ifndef OPM_ROOTFINDERS_HEADER
+#define OPM_ROOTFINDERS_HEADER
 
 
 
@@ -335,4 +335,4 @@ namespace Opm
 
 
 
-#endif // OPENRS_ROOTFINDERS_HEADER
+#endif // OPM_ROOTFINDERS_HEADER
