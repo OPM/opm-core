f2f199258ddd7bad42ee65889165d4eeffb7583e
Roland Kaufmann <roland.kaufmann@uni.no>
2013-08-01 11:26:10 +0200

    Define API version macros for other code to query
    Note the distinction between the version (1.0) and the release label
    (2013.03). The latter is not really usable to check for API
    compatibility. (See <http://www.semver.org>).

--- opm/core/version.h
+++ opm/core/version.h
@@ -2,3 +2,10 @@
  * This symbol is initialized with the built version of the library.
  */
 extern const char* const opm_core_version;
+
+/**
+ * Current API level (for use with DUNE_VERSION_xxx):
+ */
+#define OPM_CORE_MAJOR    1
+#define OPM_CORE_MINOR    0
+#define OPM_CORE_REVISION 0
