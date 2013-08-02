99eca2a247a47ebdf07787527ce1440ff5cd40a2
Roland Kaufmann <roland.kaufmann@uni.no>
2013-08-02 09:03:49 +0200

    Name #defines to be compatible with DUNE macros

--- opm/core/version.h
+++ opm/core/version.h
@@ -6,6 +6,6 @@ extern const char* const opm_core_version;
 /**
  * Current API level (for use with DUNE_VERSION_xxx):
  */
-#define OPM_CORE_MAJOR    1
-#define OPM_CORE_MINOR    0
-#define OPM_CORE_REVISION 0
+#define OPM_CORE_VERSION_MAJOR    1
+#define OPM_CORE_VERSION_MINOR    0
+#define OPM_CORE_VERSION_REVISION 0
