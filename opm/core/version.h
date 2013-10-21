823cc64c78a4d9f09e8583c2cf2ca34b8da9f79b
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2013-10-21 13:53:29 +0200

    Bump version numbers.
    Since there are a few new APIs and features, but no major changes,
    it seems proper to move from 1.0 to 1.1, even though strict
    backward compatibility cannot be expected.
    
    Label bumped to 2013.10.

--- opm/core/version.h
+++ opm/core/version.h
@@ -7,5 +7,5 @@ extern const char* const opm_core_version;
  * Current API level (for use with DUNE_VERSION_xxx):
  */
 #define OPM_CORE_VERSION_MAJOR    1
-#define OPM_CORE_VERSION_MINOR    0
+#define OPM_CORE_VERSION_MINOR    1
 #define OPM_CORE_VERSION_REVISION 0
