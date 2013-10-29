9bb4941a38f88ca814b9829f6ea17839a6f0dfe1
Roland Kaufmann <roland.kaufmann@uni.no>
2013-10-29 14:20:36 +0100

    Extract common parts of blackoil and incomp. state
    Put the identical parts of the simulator state into a base class that
    they can be referenced from when adressing the common fields.

---
+++ opm/core/simulator/SimulatorState.cpp
@@ -0,0 +1,96 @@
+#include <opm/core/simulator/SimulatorState.hpp>
+#include <opm/core/grid.h>
+
+#include <cmath>
+
+using namespace Opm;
+
+bool
+SimulatorState::equals (const SimulatorState& other,
+                        double epsilon) const {
+    bool equal = (num_phases_ == other.num_phases_);
+
+    // if we use &=, then all the tests will be run regardless
+    equal = equal && vectorApproxEqual( pressure() , other.pressure() , epsilon);
+    equal = equal && vectorApproxEqual( facepressure() , other.facepressure() , epsilon);
+    equal = equal && vectorApproxEqual( faceflux() , other.faceflux() , epsilon);
+    equal = equal && vectorApproxEqual( saturation() , other.saturation() , epsilon);
+
+    return equal;
+}
+
+bool
+SimulatorState::vectorApproxEqual(const std::vector<double>& v1,
+                                  const std::vector<double>& v2,
+                                  double epsilon) {
+    if (v1.size() != v2.size()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < v1.size(); i++) {
+        const double diff = std::abs(v1[i] - v2[i]);
+        const double scale = std::abs(v1[i]) + std::abs(v2[i]);
+        if (diff > epsilon * scale) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void
+SimulatorState::init(const UnstructuredGrid& g, int num_phases)
+{
+    num_phases_ = num_phases;
+    press_.resize(g.number_of_cells, 0.0);
+    fpress_.resize(g.number_of_faces, 0.0);
+    flux_.resize(g.number_of_faces, 0.0);
+    sat_.resize(num_phases_ * g.number_of_cells, 0.0);
+    for (int cell = 0; cell < g.number_of_cells; ++cell) {
+        // Defaulting the second saturation to 1.0.
+        // This will usually be oil in a water-oil case,
+        // gas in an oil-gas case.
+        // For proper initialization, one should not rely on this,
+        // but use available phase information instead.
+        sat_[num_phases_*cell + 1] = 1.0;
+    }
+}
+
+template <typename Props> void
+SimulatorState::setFirstSat(const std::vector<int>& cells,
+                            const Props& props,
+                            ExtremalSat es)
+{
+    if (cells.empty()) {
+        return;
+    }
+    int n = cells.size();
+    std::vector<double> smin(num_phases_*n);
+    std::vector<double> smax(num_phases_*n);
+    props.satRange(n, &cells[0], &smin[0], &smax[0]);
+    const double* svals = (es == MinSat) ? &smin[0] : &smax[0];
+    for (int ci = 0; ci < n; ++ci) {
+        const int cell = cells[ci];
+        sat_[num_phases_*cell] = svals[num_phases_*ci];
+        sat_[num_phases_*cell + 1] = 1.0 - sat_[num_phases_*cell];
+    }
+}
+
+// template instantiations for all known (to this library) subclasses
+// of SimulatorState that will call this method. notice that there are
+// no empty angle brackets after "template" -- that would have been
+// specialization instead
+#include <opm/core/props/BlackoilPropertiesInterface.hpp>
+#include <opm/core/props/IncompPropertiesInterface.hpp>
+
+template void
+SimulatorState::setFirstSat <IncompPropertiesInterface> (
+        const std::vector<int> &cells,
+        const IncompPropertiesInterface &props,
+        ExtremalSat es);
+
+template void
+SimulatorState::setFirstSat <BlackoilPropertiesInterface> (
+        const std::vector<int> &cells,
+        const BlackoilPropertiesInterface &props,
+        ExtremalSat es);
---
+++ opm/core/simulator/SimulatorState.hpp
@@ -0,0 +1,78 @@
+// Copyright (C) 2013 Uni Research AS
+// This file is licensed under the GNU General Public License v3.0
+
+#ifndef OPM_SIMULATORSTATE_HEADER_INCLUDED
+#define OPM_SIMULATORSTATE_HEADER_INCLUDED
+
+#include <vector>
+
+// forward declaration
+struct UnstructuredGrid;
+
+namespace Opm
+{
+    class SimulatorState
+    {
+    public:
+
+        virtual void init(const UnstructuredGrid& g, int num_phases);
+
+        enum ExtremalSat { MinSat, MaxSat };
+
+    protected:
+        /**
+         * Initialize the first saturation to maximum value. This method
+         * should be considered deprecated. Avoid to use it!
+         *
+         * \tparam Props Fluid and rock properties that pertain to this
+         *               kind of simulation. Currently, only Blackoil-
+         *               and IncompPropertiesInterface are supported.
+         */
+        template <typename Props>
+        void setFirstSat(const std::vector<int>& cells,
+                         const Props& props,
+                         ExtremalSat es);
+    public:
+        int numPhases() const { return num_phases_; }
+
+        std::vector<double>& pressure    () { return press_ ; }
+        std::vector<double>& facepressure() { return fpress_; }
+        std::vector<double>& faceflux    () { return flux_  ; }
+        std::vector<double>& saturation  () { return sat_   ; }
+
+        const std::vector<double>& pressure    () const { return press_ ; }
+        const std::vector<double>& facepressure() const { return fpress_; }
+        const std::vector<double>& faceflux    () const { return flux_  ; }
+        const std::vector<double>& saturation  () const { return sat_   ; }
+
+        /**
+         * Compare this state with another, to see if they are different
+         * only within a small margin.
+         */
+        virtual bool equals(const SimulatorState& other,
+                            double epsilon = 1e-8) const;
+    private:
+        int num_phases_;
+        std::vector<double> press_ ;
+        std::vector<double> fpress_;
+        std::vector<double> flux_  ;
+        std::vector<double> sat_   ;
+
+    protected:
+        /**
+         * Check if two vectors are equal within a margin.
+         *
+         * @param epsilon Relative difference that is tolerated for the
+         *                vectors to still be considered equal.
+         *
+         * @return True if every element is within the margin, false if
+         *         there is at least one that is not.
+         */
+        static bool vectorApproxEqual(const std::vector<double>& v1,
+                                      const std::vector<double>& v2,
+                                      double epsilon);
+    };
+
+} // namespace Opm
+
+#endif // OPM_SIMULATORSTATE_HEADER_INCLUDED
