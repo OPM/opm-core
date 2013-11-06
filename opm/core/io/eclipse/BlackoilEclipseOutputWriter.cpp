98a36189d376dbe698e08f2eec8eeae23cb8a3fc
Roland Kaufmann <roland.kaufmann@uni.no>
2013-11-06 13:20:37 +0100

    Conditionally use input file
    If config.h is written, then the build system will also define
    HAVE_CONFIG_H.

--- opm/core/io/eclipse/BlackoilEclipseOutputWriter.cpp
+++ opm/core/io/eclipse/BlackoilEclipseOutputWriter.cpp
@@ -18,7 +18,10 @@
   You should have received a copy of the GNU General Public License
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
  */
+#if HAVE_CONFIG_H
 #include "config.h"
+#endif // HAVE_CONFIG_H
+
 #include "BlackoilEclipseOutputWriter.hpp"
 
 #include <opm/core/grid.h>
