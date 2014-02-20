4408ea669b953e2bd9e4166fef3858fea6ca3539
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2014-02-20 15:39:15 +0100

    Add another test deck for initialisation.
    This deck includes capillary functions.

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -182,6 +182,7 @@ list (APPEND TEST_DATA_FILES
   tests/testBlackoilState1.DATA
   tests/testBlackoilState2.DATA
   tests/wells_manager_data.data
+	tests/capillary.DATA
 	tests/deadfluids.DATA
 	)
 
