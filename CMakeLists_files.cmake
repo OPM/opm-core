7281a9567f5c420b0dc5debbc812cccf0b513268
Markus Blatt <markus@dr-blatt.de>
2014-03-19 21:32:36 +0100

    Added a test for the linear solver interface.
    This test sets up a simple laplace problem and solves it with the available
    solvers. It assume that either dune-istl or UMFPack is present, which is
    assume to be safe.

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -162,6 +162,7 @@ list (APPEND TEST_SOURCE_FILES
 	tests/test_wachspresscoord.cpp
 	tests/test_column_extract.cpp
 	tests/test_geom2d.cpp
+	tests/test_linearsolver.cpp
 	tests/test_param.cpp
 	tests/test_blackoilfluid.cpp
 	tests/test_shadow.cpp
