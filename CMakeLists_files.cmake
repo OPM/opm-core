c2f03bbc80e74573f1da21c180703d42eb9f1d5f
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2014-08-27 18:09:27 +0200

    Add new file.

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -408,6 +408,7 @@ list (APPEND PUBLIC_HEADER_FILES
 	opm/core/utility/parameters/tinyxml/tinyxml.h
 	opm/core/utility/PropertySystem.hpp
 	opm/core/utility/share_obj.hpp
+	opm/core/utility/thresholdPressures.hpp
 	opm/core/wells/InjectionSpecification.hpp
 	opm/core/wells/ProductionSpecification.hpp
 	opm/core/wells/WellCollection.hpp
