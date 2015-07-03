3bc520eeb7a9fe43f0c97025a26fcc7f3dfbdcef
Robert Kloefkorn <robert.kloefkorn@iris.no>
2015-07-03 16:26:59 +0200

    SimulatorState: store all data in a vector to allow for flexibility when used with derived states. BlackoilState: adjusted to SimulatorState changes.

--- opm/core/simulator/SimulatorState.cpp
+++ opm/core/simulator/SimulatorState.cpp
@@ -2,6 +2,7 @@
 #include <opm/core/grid.h>
 
 #include <cmath>
+#include <cassert>
 
 using namespace Opm;
 
@@ -48,19 +49,61 @@ void
 SimulatorState::init(int number_of_cells, int number_of_faces, int num_phases)
 {
     num_phases_ = num_phases;
-    press_.resize(number_of_cells, 0.0);
-    temp_.resize(number_of_cells, 273.15 + 20);
-    fpress_.resize(number_of_faces, 0.0);
-    flux_.resize(number_of_faces, 0.0);
-    sat_.resize(num_phases_ * number_of_cells, 0.0);
+
+    cellData_.resize( 3 );
+    faceData_.resize( 2 );
+
+    pressure().resize(number_of_cells, 0.0);
+    temperature().resize(number_of_cells, 273.15 + 20);
+    saturation().resize(num_phases_ * number_of_cells, 0.0);
+
+    cellDataNames_.resize( 3 );
+    cellDataNames_[ pressureId_ ] = "PRESSURE" ;
+    cellDataNames_[ temperatureId_ ] = "TEMPERATURE";
+    cellDataNames_[ saturationId_ ] = "SATURATION" ;
+
     for (int cell = 0; cell < number_of_cells; ++cell) {
         // Defaulting the second saturation to 1.0.
         // This will usually be oil in a water-oil case,
         // gas in an oil-gas case.
         // For proper initialization, one should not rely on this,
         // but use available phase information instead.
-        sat_[num_phases_*cell + 1] = 1.0;
+        saturation()[num_phases_*cell + 1] = 1.0;
     }
+
+    facepressure().resize(number_of_faces, 0.0);
+    faceflux().resize(number_of_faces, 0.0);
+
+    faceDataNames_.resize( 2 );
+    faceDataNames_[ facePressureId_ ] = "FACEPRESSURE" ;
+    faceDataNames_[ faceFluxId_ ] =  "FACEFLUX" ;
+
+}
+
+size_t
+SimulatorState::registerCellData( const std::string& name, const int components, const double initialValue )
+{
+    // check if init has been called
+    assert( cellData_.size() > 0 );
+    const size_t pos = cellData_.size();
+    cellDataNames_.push_back( name );
+    const size_t nCells = pressure().size();
+    assert( nCells > 0 );
+    cellData_.push_back( std::vector<double>( nCells * components, initialValue ) );
+    return pos;
+}
+
+size_t
+SimulatorState::registerFaceData( const std::string& name, const int components, const double initialValue )
+{
+    // check if init has been called
+    assert( faceData_.size() > 0 );
+    const size_t pos = faceData_.size();
+    faceDataNames_.push_back( name );
+    const size_t nFaces = facepressure().size();
+    assert( nFaces > 0 );
+    faceData_.push_back( std::vector<double>( nFaces * components, initialValue ) );
+    return pos ;
 }
 
 template <typename Props> void
@@ -75,11 +118,12 @@ SimulatorState::setFirstSat(const std::vector<int>& cells,
     std::vector<double> smin(num_phases_*n);
     std::vector<double> smax(num_phases_*n);
     props.satRange(n, &cells[0], &smin[0], &smax[0]);
+    std::vector< double >& sat = saturation();
     const double* svals = (es == MinSat) ? &smin[0] : &smax[0];
     for (int ci = 0; ci < n; ++ci) {
         const int cell = cells[ci];
-        sat_[num_phases_*cell] = svals[num_phases_*ci];
-        sat_[num_phases_*cell + 1] = 1.0 - sat_[num_phases_*cell];
+        sat[num_phases_*cell] = svals[num_phases_*ci];
+        sat[num_phases_*cell + 1] = 1.0 - sat[num_phases_*cell];
     }
 }
 
