8ef2a923ece36f594d553740d8e7a7a5376a8b58
Andreas Lauser <and@poware.org>
2017-02-07 12:48:45 +0100

    fix a GCC 7 warning
    GCC 7 warns
    
    ```
    dynamic exception specifications are deprecated in C++11; use ‘noexcept’ instead [-Wdeprecated]
    ```
    
    here.

--- opm/core/utility/MonotCubicInterpolator.cpp
+++ opm/core/utility/MonotCubicInterpolator.cpp
@@ -166,7 +166,7 @@ read(const std::string & datafilename, int xColumn, int fColumn)
 
 void
 MonotCubicInterpolator::
-addPair(double newx, double newf) throw(const char*) {
+addPair(double newx, double newf) {
   if (std::isnan(newx) || std::isinf(newx) || std::isnan(newf) || std::isinf(newf)) {
     throw("MonotCubicInterpolator: addPair() received inf/nan input.");
   }
@@ -181,7 +181,7 @@ addPair(double newx, double newf) throw(const char*) {
 
 double
 MonotCubicInterpolator::
-evaluate(double x) const throw(const char*){
+evaluate(double x) const {
 
   if (std::isnan(x) || std::isinf(x)) {
     throw("MonotCubicInterpolator: evaluate() received inf/nan input.");
@@ -303,7 +303,7 @@ toString() const
 
 pair<double,double>
 MonotCubicInterpolator::
-getMissingX() const throw(const char*)
+getMissingX() const
 {
   if( data.size() < 2) {
     throw("MonotCubicInterpolator::getMissingX() only one datapoint.");
@@ -337,7 +337,7 @@ getMissingX() const throw(const char*)
 
 pair<double,double>
 MonotCubicInterpolator::
-getMaximumF() const throw(const char*) {
+getMaximumF() const {
   if (data.size() <= 1) {
     throw ("MonotCubicInterpolator::getMaximumF() empty data.") ;
   }
@@ -360,7 +360,7 @@ getMaximumF() const throw(const char*) {
 
 pair<double,double>
 MonotCubicInterpolator::
-getMinimumF() const throw(const char*) {
+getMinimumF() const {
   if (data.size() <= 1) {
     throw ("MonotCubicInterpolator::getMinimumF() empty data.") ;
   }
