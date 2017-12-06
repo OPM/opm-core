febdc04e806b7aa5faa86efc8a649549f6374102
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2017-12-06 12:07:33 +0100

    remove tutorials which have been moved to opm-simulators

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -156,17 +156,13 @@ list (APPEND TEST_DATA_FILES
         )
 
 # originally generated with the command:
-# find tutorials examples -name '*.c*' -printf '\t%p\n' | sort
+# find examples -name '*.c*' -printf '\t%p\n' | sort
 list (APPEND EXAMPLE_SOURCE_FILES
 	examples/compute_eikonal_from_files.cpp
 	examples/compute_initial_state.cpp
 	examples/compute_tof.cpp
 	examples/compute_tof_from_files.cpp
 	examples/diagnose_relperm.cpp
-	tutorials/tutorial1.cpp
-	tutorials/tutorial2.cpp
-	tutorials/tutorial3.cpp
-	tutorials/tutorial4.cpp
 	)
 
 # originally generated with the command:
